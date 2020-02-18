bb8216735950dd968e31730c70ab15fbf2ea504b
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-18 16:32:21 +0300

    setOnFocusChangeListener

--- app/src/main/java/com/bankmtk/textinputlayout/MainActivity.java
+++ app/src/main/java/com/bankmtk/textinputlayout/MainActivity.java
@@ -3,6 +3,8 @@ package com.bankmtk.textinputlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -20,5 +22,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.inputLoginName);
+        password = findViewById(R.id.inputPassword);
+
+        login.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) return;
+                TextView tv = (TextView)v;
+                validate(tv,checkLogin,"It's not name!");
+            }
+        });
+        password.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus)return;
+                TextView tv = (TextView)v;
+                validate(tv,checkPassword,"Very simple password!");
+            }
+        });
+    }
+    private void validate(TextView tv, Pattern check, String message){
+        String value = tv.getText().toString();
+        if (check.matcher(value).matches()){
+            hideError(tv);
+        }
+        else {
+            showError(tv,message);
+        }
+    }
+    private void showError(TextView view,String message){
+        view.setError(message);
+    }
+    private void hideError(TextView view){
+        view.setError(null);
     }
 }
