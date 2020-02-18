d538f4b53606e7dce91bf56e1b54c923f96a28c2
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-18 16:18:24 +0300

    add Pattern for login

--- app/src/main/java/com/bankmtk/textinputlayout/MainActivity.java
+++ app/src/main/java/com/bankmtk/textinputlayout/MainActivity.java
@@ -4,7 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
+    TextInputEditText login;
+    TextInputEditText password;
+
+    Pattern checkLogin = Pattern.compile("^[A-Z][a-z]{2,}$");
+    Pattern checkPassword = Pattern.compile("^(?=^.{6,}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s).*$");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
